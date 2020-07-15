e9c3ee4710f3690f62b10e4b400a37a58daa7060
bigbadbloo <agapovefim@gmail.com>
2020-07-16 00:12:48 +0300

    git init resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,8 +3,20 @@ import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
      static className = 'excel__table';
-
+    constructor($root) {
+        super($root, {
+        listeners: ['click', 'mousedown', 'mouseup', 'mousemove'],
+    });
+    }
      toHTML() {
        return createTable();
      }
+     onClick(){
+
+     }
+
+     onMousedown(){
+
+     }
+
 }
