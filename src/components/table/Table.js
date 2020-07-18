ee768998de560f530281f45bf5fb11630322e613
bigbadbloo <agapovefim@gmail.com>
2020-07-19 00:24:31 +0300

    git finish resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {shouldResize} from "@/components/table/table.functions";
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
      static className = 'excel__table';
